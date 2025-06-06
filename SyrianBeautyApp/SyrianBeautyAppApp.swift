8ac5a2068aad626a42047a5625b13cc37e42f5dc
ALI <alinasserkhazali@gmail.com>
2025-06-06 10:39:35 +0300

    Initial Commit

---
+++ SyrianBeautyApp/SyrianBeautyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyrianBeautyAppApp.swift
+//  SyrianBeautyApp
+//
+//  Created by Ali Al-Khazali on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SyrianBeautyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
